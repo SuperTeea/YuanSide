9e9af64cfbbd667543d52e09c1e93a52147c87ae
SuperTeea <super_teaer@qq.com>
2025-03-21 09:53:50 +0800

    InventoryInit

--- Super_Tea/Player/Script/Player.gd
+++ Super_Tea/Player/Script/Player.gd
@@ -10,11 +10,7 @@ var curAnimationpos = 0
 @onready var animation : AnimationPlayer = $AnimationPlayer
 
 func _process(delta: float) -> void:
-	
-	direction.x = Input.get_action_strength('right') - Input.get_action_strength('left')
-	direction.y = Input.get_action_strength('down') - Input.get_action_strength('up')
-	direction = direction.normalized()
-	
+	direction = Input.get_vector('left', 'right', 'up', 'down')
 	velocity = direction * move_speed
 	
 	if SetState() || SetDir():
