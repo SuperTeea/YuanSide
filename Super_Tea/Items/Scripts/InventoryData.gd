29b26e0bbbf4cf22c9e1e286fbbd5ded98cd7464
SuperTeea <super_teaer@qq.com>
2025-03-24 20:11:12 +0800

    inventory modify

--- Super_Tea/Items/Scripts/InventoryData.gd
+++ Super_Tea/Items/Scripts/InventoryData.gd
@@ -1,1 +1,6 @@
 class_name InventoryData extends Resource
+
+@export var slots : Array[ SlotData ]
+
+func add_item() -> void:
+	pass
---
+++ Super_Tea/Items/Scripts/SlotData.gd
@@ -0,0 +1,4 @@
+class_name SlotData extends Resource
+
+@export var itemData : ItemData
+@export var quantity : int = 0
